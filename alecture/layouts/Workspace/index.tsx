a669427bdf521c85fdee7e7443f16f75185dbc30
lsj0202 <w1004123@gmail.com>
2023-09-11 12:56:10 +0900

    workspace 구조잡기

--- alecture/layouts/Workspace/index.tsx
+++ alecture/layouts/Workspace/index.tsx
@@ -3,25 +3,48 @@ import axios from 'axios';
 import React, { FC, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import useSWR from 'swr';
+import gravatar from 'gravatar';
+import {
+  AddButton,
+  Channels,
+  Chats,
+  Header,
+  LogOutButton,
+  MenuScroll,
+  ProfileImg,
+  ProfileModal,
+  RightMenu,
+  WorkspaceButton,
+  WorkspaceModal,
+  WorkspaceName,
+  Workspaces,
+  WorkspaceWrapper,
+} from './style';
 
 const Workspace: FC = ({ children }) => {
-  const { data, error, revalidate } = useSWR('/api/users', fetcher);
-  const onLogout = useCallback(() => {
-    axios
-      .post('http://localhost:3095/api/users/logout', null, {
-        withCredentials: true,
-      }) // 쿠키 공유
-      .then(() => {
-        revalidate(); // 스토리지(쿠키) 삭제
-      });
-  }, []);
   return (
-    <>
-      <button onClick={onLogout}>
-        <Link to="/login">로그아웃</Link>
-      </button>
-      <div>{children}</div>
-    </>
+    <div>
+      <Header>
+        <RightMenu>
+          <span>
+            <ProfileImg
+              src={gravatar.url('lsj', {
+                s: '28px',
+                d: 'retro',
+              })}
+              alt={'nickname'}
+            />
+          </span>
+        </RightMenu>
+      </Header>
+      <WorkspaceWrapper>
+        <Workspace>test</Workspace>
+        <Channels>
+          <WorkspaceName>Sleact</WorkspaceName>
+        </Channels>
+        <Chats>Chats</Chats>
+      </WorkspaceWrapper>
+    </div>
   );
 };
 
